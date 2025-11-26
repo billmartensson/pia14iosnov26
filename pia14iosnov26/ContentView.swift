0add548d6bad5581f32b1302c2cb1d9b88fac65f
Bill Mårtensson <bill@magictechnology.se>
2025-11-26 11:30:46 +0100

    shop

--- pia14iosnov26/ContentView.swift
+++ pia14iosnov26/ContentView.swift
@@ -11,19 +11,62 @@ struct ContentView: View {
     
     @State var shopcode = ShoppingCode()
     
+    @State var addname = ""
+    @State var addamount = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack {
+
+                TextField("Name", text: $addname)
+                    .background(Color.red)
+
+                TextField("Amount", text: $addamount)
+                    .background(Color.green)
+                    .frame(width: 50)
+
+                Button("ADD") {
+                    shopcode.addShop(addname: addname, addamount: addamount)
+                }
+            }
+            
+            if shopcode.isError {
+                VStack {
+                    Text("FEL FEL ERROR ERROR")
+                }
+                .padding()
+                .background(Color.red)
+            }
             
             List(shopcode.shopping) { shopthing in
-                HStack {
-                    Text(shopthing.name)
-                    Spacer()
-                    Text("\(shopthing.amount)")
+                
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Text(shopthing.name)
+                        Spacer()
+                        Text("\(shopthing.amount)")
+                        
+                        Button(action: {
+                            shopcode.changeDone(changething: shopthing)
+                        }) {
+                            if shopthing.isDone {
+                                Text("✅")
+                            } else {
+                                Text("☑️")
+                            }
+                        }
+                        
+                        Button(action: {
+                            shopcode.deleteShop(deletething: shopthing)
+                        }) {
+                            Text("🗑️")
+                        }
+                    }
                 }
+                
             }
             
         }
--- pia14iosnov26/ShoppingCode.swift
+++ pia14iosnov26/ShoppingCode.swift
@@ -19,12 +19,45 @@ struct Shopitem : Identifiable {
         
     var shopping : [Shopitem] = []
     
+    var isError = false
+    
     func loadshopping() {
         
-        var s1 = Shopitem(name: "Apelsin", amount: 10)
-        var s2 = Shopitem(name: "Banan", amount: 12)
+        let s1 = Shopitem(name: "Apelsin", amount: 10)
+        let s2 = Shopitem(name: "Banan", amount: 12)
         
         shopping.append(s1)
         shopping.append(s2)
     }
+    
+    func addShop(addname : String, addamount : String) {
+        
+        guard let amountnumber = Int(addamount) else {
+            isError = true
+            return
+        }
+        
+        guard addname != "" else {
+            isError = true
+            return
+        }
+                
+        let s1 = Shopitem(name: addname, amount: amountnumber)
+
+        shopping.append(s1)
+
+        isError = false
+    }
+    
+    func deleteShop(deletething : Shopitem) {
+        shopping.removeAll(where: { $0.id == deletething.id })
+    }
+    
+    func changeDone(changething : Shopitem) {
+        if let shopindex = shopping.firstIndex(where: { $0.id == changething.id }) {
+            
+            shopping[shopindex].isDone.toggle()
+        }
+    }
+    
 }
